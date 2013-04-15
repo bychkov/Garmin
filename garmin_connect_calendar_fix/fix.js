8afbcf5dbbccf784373d29f05a73d4d800769ac5
Vladimir Bychkov <bychkov@gmail.com>
2013-04-15 12:18:11 -0400

    v1 - update
    v1 - update

--- garmin_connect_calendar_fix/fix.js
+++ garmin_connect_calendar_fix/fix.js
@@ -1,26 +1,28 @@
-$("table.calendar-table").ready(function() {
+CalendarFix = function() {
+    setTimeout(function(){
+        jQuery(".item-icon.icon-run").parent().css({"background-color": "lightblue", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-swim").parent().css({"background-color": "gold", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-cycle").parent().css({"background-color": "lightcoral", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-fitness").parent().css({"background-color": "mediumpurple", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-hike").parent().css({"background-color": "peru", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-walk").parent().css({"background-color": "yellowgreen", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-other").parent().css({"background-color": "moccasin", "color" : "black", "font-weight" : "bold" });
+        jQuery(".item-icon.icon-transition").parent().css({"background-color": "orangered", "color" : "black", "font-weight" : "bold" });
 
-setTimeout(function(){
+        jQuery(".calendar-month .calendar-item").css("white-space", "normal");
 
-jQuery(".item-icon.icon-run").parent().css({"background-color": "lightblue", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-swim").parent().css({"background-color": "gold", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-cycle").parent().css({"background-color": "lightcoral", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-fitness").parent().css({"background-color": "mediumpurple", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-hike").parent().css({"background-color": "peru", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-walk").parent().css({"background-color": "yellowgreen", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-other").parent().css({"background-color": "moccasin", "color" : "black", "font-weight" : "bold" });
-jQuery(".item-icon.icon-transition").parent().css({"background-color": "orangered", "color" : "black", "font-weight" : "bold" });
+        jQuery("div.calendar-item div.item-content").each(function(index) { 
+            var activityTitle = jQuery(this).parent().attr("title");
+            var currentText = jQuery.trim(jQuery(this).text()); 
+            jQuery(this).text(activityTitle + " - " + currentText); 
+        });
 
-jQuery(".calendar-month .calendar-item").css("white-space", "normal");
+    }, 3000);
+};
 
-jQuery("div.calendar-item div.item-content").each(function(index) { 
-	var activityTitle = jQuery(this).parent().attr("title");
-	var currentText = jQuery.trim(jQuery(this).text()); 
-	jQuery(this).text(activityTitle + " - " + currentText); 
-});
+jQuery("table.calendar-table").ready(function() {
+	CalendarFix();
+	jQuery(".calendar-goto-previous").bind("click", function() { CalendarFix(); } );
+	jQuery(".calendar-goto-next").bind("click", function() { CalendarFix(); } );
+    });
 
-
-}, 3000);
-
-    console.log("I have been loaded!");
-});
